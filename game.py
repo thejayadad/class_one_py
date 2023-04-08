5eaf41da321f17cf6ac0607f17c44cc21cdd9124
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-08 17:35:13 -0400

    update

--- game.py
+++
@@ -1,8 +0,0 @@
-
-
-balance = 0
-
-print("***********WELCOME TO SLOTS*****************")
-deposit = input("How much do you want to deposit? ")
-balance += deposit
-print(balance)
--- main.py
+++ main.py
@@ -512,10 +512,5 @@
 #<---static and instance methods----------------->
 
 
-
-balance = 0
-
-print("***********WELCOME TO SLOTS*****************")
-deposit = input("How much do you want to deposit? ")
-balance += deposit
-print(balance)
+x = input("name")
+print(x)
